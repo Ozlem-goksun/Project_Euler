718d10d9589f421f7e7078ec5740db20f2c4f69a
dinosOnVenus <ozlemgoksun1389@gmail.com>
2024-11-20 12:19:59 +0300

    solved the problem2

--- Problem2.c
+++ Problem2.c
@@ -3,3 +3,30 @@
 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 
 By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms. */
+
+#include <stdio.h>
+
+int main()
+{
+	int num, n1 = 0, n2 = 1, sumOfEvens = 0, temp = 0;
+	
+	scanf("%d", &num);
+	
+	while(temp < num)
+	{
+		temp = n1 + n2;
+		
+		if(temp % 2 == 0)
+		{
+			sumOfEvens = sumOfEvens + temp;
+		}
+		
+		n1 = n2;
+		n2 = temp;
+	}
+	
+	printf("%d", sumOfEvens);
+	
+	
+	return 0;
+}
